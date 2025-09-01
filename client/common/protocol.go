80dd91eaec70405d7a8b641e34708dd93bd7f139
tinchomorilla <mmorilla@fi.uba.ar>
2025-09-01 14:13:18 -0300

    refactor: move message types and related structures from protocol.go to messages.go

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -8,83 +8,16 @@ import (
 	"strings"
 )
 
-// MessageType represents the type of message being sent
-type MessageType byte
-
-const (
-	MessageTypeBet        MessageType = 1
-	MessageTypeBatch      MessageType = 2
-	MessageTypeResponse   MessageType = 3
-	MessageTypeGetWinners MessageType = 4
-)
-
-// BetMessage represents a betting request from client to server
-type BetMessage struct {
-	Type       MessageType
-	Nombre     string
-	Apellido   string
-	Documento  string
-	Nacimiento string
-	Numero     int
-}
-
-// BatchMessage represents multiple bets sent together
-type BatchMessage struct {
-	Type   MessageType
-	Agency int // Agency number (1-5)
-	Bets   []BetMessage
-	EOF    bool
-}
-
-// ResponseMessage represents server response to client
-type ResponseMessage struct {
-	Type    MessageType
-	Success bool
-	Error   string
-	Winners []string // List of winner DNIs
-}
-
-// GetWinnersMessage represents a request to get winners for an agency
-type GetWinnersMessage struct {
-	Type   MessageType
-	Agency int // Agency number (1-5)
-}
-
-// NewBetMessage creates a new bet message from the provided data
-func NewBetMessage(nombre, apellido, documento, nacimiento string, numero int) *BetMessage {
-	return &BetMessage{
-		Type:       MessageTypeBet,
-		Nombre:     nombre,
-		Apellido:   apellido,
-		Documento:  documento,
-		Nacimiento: nacimiento,
-		Numero:     numero,
-	}
-}
-
-// NewBatchMessage creates a new batch message from a slice of bets
-func NewBatchMessage(agency int, bets []BetMessage, eof bool) *BatchMessage {
-	return &BatchMessage{
-		Type:   MessageTypeBatch,
-		Agency: agency,
-		Bets:   bets,
-		EOF:    eof,
-	}
-}
-
-// NewGetWinnersMessage creates a new get winners message
-func NewGetWinnersMessage(agency int) *GetWinnersMessage {
-	return &GetWinnersMessage{
-		Type:   MessageTypeGetWinners,
-		Agency: agency,
-	}
-}
-
 // SerializeMessage converts a message to custom protocol format
 func SerializeMessage(message interface{}) ([]byte, error) {
 	var data []byte
 
 	switch msg := message.(type) {
+	case *BetMessage:
+		data = append(data, byte(MessageTypeBet))
+		content := fmt.Sprintf("%s|%s|%s|%s|%d", msg.Nombre, msg.Apellido, msg.Documento, msg.Nacimiento, msg.Numero)
+		data = append(data, []byte(content)...)
+
 	case *BatchMessage:
 		data = append(data, byte(MessageTypeBatch))
 		content := fmt.Sprintf("%d|%d|%d", msg.Agency, boolToInt(msg.EOF), len(msg.Bets))
@@ -152,14 +85,6 @@ func parseResponseMessage(content string) (*ResponseMessage, error) {
 	}, nil
 }
 
-// boolToInt converts boolean to integer
-func boolToInt(b bool) int {
-	if b {
-		return 1
-	}
-	return 0
-}
-
 // SendMessage serializes message and sends with length prefix
 func SendMessage(conn net.Conn, message interface{}) error {
 	data, err := SerializeMessage(message)
@@ -167,9 +92,9 @@ func SendMessage(conn net.Conn, message interface{}) error {
 		return fmt.Errorf("error serializing message: %w", err)
 	}
 
-	// Send length prefix (4 bytes, big endian)
+	// Send length prefix
 	length := uint32(len(data))
-	lengthBytes := make([]byte, 4)
+	lengthBytes := make([]byte, LENGTH_PREFIX_BYTES)
 	binary.BigEndian.PutUint32(lengthBytes, length)
 
 	if _, err := conn.Write(lengthBytes); err != nil {
@@ -186,8 +111,8 @@ func SendMessage(conn net.Conn, message interface{}) error {
 
 // RecvMessage reads length-prefixed message and deserializes
 func RecvMessage(conn net.Conn, v interface{}) error {
-	// Read length prefix (4 bytes)
-	lengthBytes := make([]byte, 4)
+	// Read length prefix
+	lengthBytes := make([]byte, LENGTH_PREFIX_BYTES)
 	if _, err := readExact(conn, lengthBytes); err != nil {
 		return fmt.Errorf("error reading length: %w", err)
 	}
